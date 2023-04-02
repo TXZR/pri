c1518adb7d9eb36ec591a2ded7a3a6bb3bd3d85c
Seands <seands@seands.cn>
2023-04-02 15:02:45 +0800

    update style

--- src/components/Query.vue
+++ src/components/Query.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <el-form ref="form" :model="form" :label-position="top">
-      <el-form-item label="Destination:" :rules="[
+    <el-form ref="form" :model="form" :label-position="'right'" label-width="200px">
+      <el-form-item label="Destination:" prop="destination" :rules="[
         { required: true, message: 'Destination不能为空'},
       ]">
         <el-select v-model="form.destination" filterable placeholder="请选择" >
@@ -13,20 +13,18 @@
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="Earliest delivery date:" :rules="[
+      <el-form-item label="Earliest delivery date:" prop="edDate" :rules="[
         { required: true, message: 'Date不能为空'},
       ]">
-        <div class="grid-content bg-purple">
-          <el-date-picker
-              v-model="form.edDate"
-              type="date"
-              @change="edDateChange"
-              value-format="yyyy-MM-dd"
-              placeholder="选择日期">
-          </el-date-picker>
-        </div>
+        <el-date-picker
+            v-model="form.edDate"
+            type="date"
+            @change="edDateChange"
+            value-format="yyyy-MM-dd"
+            placeholder="选择日期">
+        </el-date-picker>
       </el-form-item>
-      <el-form-item label="Commodity:" :rules="[
+      <el-form-item label="Commodity:" prop="commodity" :rules="[
         { required: true, message: 'Commodity不能为空'},
       ]">
         <el-select v-model="form.commodity" filterable placeholder="请选择" @change=commodityChange>
@@ -39,7 +37,7 @@
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="Speed:" :rules="[
+      <el-form-item label="Speed:" prop="speed" :rules="[
         { required: true, message: 'Speed不能为空'},
       ]">
         <el-select v-model="form.speed" filterable placeholder="请选择">
@@ -51,31 +49,37 @@
           </el-option>
         </el-select>
       </el-form-item>
-      <label label="Overall shipment information:">Overall shipment information:</label>
-      <el-form-item label="Total no. of pieces:" :rules="[
+      <div style="width: 100%; text-align: center;">
+        <label label="Overall shipment information:">Overall shipment information:</label>
+      </div>
+      
+      <el-form-item label="Total no. of pieces:" prop="piece" :rules="[
         { required: true, message: 'pieces不能为空'},
         { type: 'number', message: 'pieces必须为数字值'}
       ]">
-        <el-input type="number" v-model="form.piece"></el-input>
+        <el-input type="number" min="1" v-model.number="form.piece"></el-input>
       </el-form-item>
-      <el-form-item label="Total weight (kg):" :rules="[
+      <el-form-item label="Total weight (kg):" prop="weight" :rules="[
         { required: true, message: 'weight不能为空'},
-        { type: 'number', message: 'weight必须为数字值'}
+        { type: 'number', message: 'weight必须为数字值'},
       ]">
-        <el-input type="number" v-model="form.weight"></el-input>
+        <el-input type="number" v-model.number="form.weight"></el-input>
       </el-form-item>
-      <el-form-item label="Total volume (mc):" :rules="[
+      <el-form-item label="Total volume (mc):" prop="volume" :rules="[
         { required: true, message: 'volume不能为空'},
         { type: 'number', message: 'volume必须为数字值'}
       ]">
-        <el-input type="number" v-model="form.volume"></el-input>
+        <el-input type="number" v-model.number="form.volume"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-checkbox v-model="form.dryIce">Dry ice</el-checkbox>
+        <el-checkbox v-model="form.dryIce" border>Dry ice</el-checkbox>
       </el-form-item>
     </el-form>
 
-    <el-button type="primary" @click="query">查询</el-button>
+    <div style="width: 100%; text-align: center;">
+      <el-button type="primary" @click="query">查询</el-button>
+    </div>
+    
 
     <br>
     <el-table
@@ -90,6 +94,7 @@
           prop="carrier"
           label="Carrier"
           sortable
+          :formatter="carrierFormat"
           width="180">
       </el-table-column>
       <el-table-column
@@ -113,7 +118,6 @@
           sortable
           width="180">
       </el-table-column>
-
       <el-table-column
           prop="arrival"
           label="Arrival(TOA)"
@@ -224,18 +228,20 @@ export default {
     },
     query() {
       this.$refs["form"].validate((valid) => {
+        console.log("valid: " + valid)
         if (valid) {
           let loadingInstance = Loading.service({fullscreen: true});
+          console.log(axios, loadingInstance)
           try {
             const params = {
-              "destination": this.destination,
-              "edDate": this.dateFormat(this.edDate, 'yyyy-MM-dd'),
-              "commodity": this.commodity,
-              "speed": this.speed,
-              "piece": parseFloat(this.piece),
-              "weight": parseFloat(this.weight),
-              "volume": parseFloat(this.volume),
-              "dryIce": this.dryIce
+              "destination": this.form.destination,
+              "edDate": this.dateFormat(this.form.edDate, 'yyyy-MM-dd'),
+              "commodity": this.form.commodity,
+              "speed": this.form.speed,
+              "piece": parseInt(this.form.piece),
+              "weight": parseFloat(this.form.weight),
+              "volume": parseFloat(this.form.volume),
+              "dryIce": this.form.dryIce
             }
             console.log(params)
             axios.post('/api/query', params)
@@ -256,12 +262,17 @@ export default {
           return false;
         }
       });
+    },
+    carrierFormat(row) {
+      return row["carrier"].join("\n")
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style>
+.el-table .cell{
+  white-space: pre-line !important;
+}
 </style>
